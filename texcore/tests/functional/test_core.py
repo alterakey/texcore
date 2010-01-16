d80ba1cea5878fa2e67674d00f04431160d94102
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2010-01-16 11:26:07 +1000

    Removing inherently broken tests

--- texcore/tests/functional/test_core.py
+++ texcore/tests/functional/test_core.py
@@ -1,8 +1,4 @@
 from texcore.tests import *
 
 class TestCoreController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='core', action='index'))
-        # Test response...
-        assert 'Hello world' in response
+    pass
